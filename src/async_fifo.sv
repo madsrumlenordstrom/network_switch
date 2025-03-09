dcf88541acd78121a30608a7091bdfa321a7cd9d
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-03-10 00:17:15 +0100

    async fifo testbench and slight fifo rework

--- src/async_fifo.sv
+++ src/async_fifo.sv
@@ -9,26 +9,28 @@ module async_fifo #(
     input logic rclk,
     input logic write_enable,
     input logic read_enable,
-    output logic [ADDR_WIDTH:0] fifo_occu_in, // Check width
-    output logic [ADDR_WIDTH:0] fifo_occu_out, // Check width
+    output logic full,
+    output logic empty,
     input logic [DATA_WIDTH-1:0] write_data_in,
     output logic [DATA_WIDTH-1:0] read_data_out
 );
+    localparam PTR_WIDTH = ADDR_WIDTH + 1;
+
     // Write clock domain signals
-    logic wen;
     logic [ADDR_WIDTH-1:0] waddr;
-    logic [ADDR_WIDTH:0] wptr;
-    logic [ADDR_WIDTH:0] rptr_sync;
+    logic [PTR_WIDTH-1:0] wptr;
+    logic [PTR_WIDTH-1:0] rptr_sync;
+    logic [ADDR_WIDTH-1:0] raddr_sync;
 
     // Read clock domain signals
-    logic ren;
     logic [ADDR_WIDTH-1:0] raddr;
-    logic [ADDR_WIDTH:0] rptr;
-    logic [ADDR_WIDTH:0] wptr_sync;
+    logic [PTR_WIDTH-1:0] rptr;
+    logic [PTR_WIDTH-1:0] wptr_sync;
+    logic [ADDR_WIDTH-1:0] waddr_sync;
 
     // Synchronizers
     data_synchronizer #(
-        ADDR_WIDTH + 1
+        PTR_WIDTH
     ) wptr_synchronizer (
         .data_in(wptr),
         .clk_in(wclk),
@@ -37,7 +39,7 @@ module async_fifo #(
     );
 
     data_synchronizer #(
-        ADDR_WIDTH + 1
+        PTR_WIDTH
     ) rptr_synchronizer (
         .data_in(rptr),
         .clk_in(rclk),
@@ -47,33 +49,24 @@ module async_fifo #(
 
     logic [DATA_WIDTH-1:0] ram [0:(1<<ADDR_WIDTH)-1];
 
-    logic full;
-    logic empty;
-    logic fifo_occupancy;
-
     always_comb begin
         waddr = wptr[ADDR_WIDTH-1:0];
         raddr = rptr[ADDR_WIDTH-1:0];
-        // full = (waddr == raddr) && (wptr[ADDR_WIDTH] != rptr[ADDR_WIDTH]); // maybe remove since not sync
-        // empty = (waddr == raddr) && (wptr[ADDR_WIDTH] == rptr[ADDR_WIDTH]); // maybe remove since not sync
-        if (wptr[ADDR_WIDTH] == rptr_sync[ADDR_WIDTH]) begin
-            fifo_occu_in = wptr[ADDR_WIDTH-1:0] - rptr_sync[ADDR_WIDTH-1:0];
-        end else begin
-            fifo_occu_in = (1<<ADDR_WIDTH) - (rptr_sync[ADDR_WIDTH-1:0] - wptr[ADDR_WIDTH-1:0]);
-        end
 
-        if (wptr_sync[ADDR_WIDTH] == rptr[ADDR_WIDTH]) begin
-            fifo_occu_out = wptr_sync[ADDR_WIDTH-1:0] - rptr[ADDR_WIDTH-1:0];
-        end else begin
-            fifo_occu_out = (1<<ADDR_WIDTH) - (rptr[ADDR_WIDTH-1:0] - wptr_sync[ADDR_WIDTH-1:0]);
-        end
+        waddr_sync = wptr_sync[ADDR_WIDTH-1:0];
+        raddr_sync = rptr_sync[ADDR_WIDTH-1:0];
+
+        full = (waddr == raddr_sync) && (wptr[PTR_WIDTH-1] != rptr_sync[PTR_WIDTH-1]);
+        empty = (waddr_sync == raddr) && (wptr_sync[PTR_WIDTH-1] == rptr[PTR_WIDTH-1]);
+
+        read_data_out = ram[raddr];
     end
 
     // Write operation
     always_ff @(posedge wclk or posedge reset) begin
         if (reset) begin
             wptr <= 0;
-        end else if (wen) begin
+        end else if (write_enable && !full) begin
             ram[waddr] <= write_data_in;
             wptr <= wptr + 1;
         end
@@ -83,10 +76,7 @@ module async_fifo #(
     always_ff @(posedge rclk or posedge reset) begin
         if (reset) begin
             rptr <= 0;
-        end else if (ren) begin
-            // Maybe only rptr should be updated in
-            // Does is make sense to put read data in register???
-            read_data_out <= ram[raddr];
+        end else if (read_enable && !empty) begin
             rptr <= rptr + 1;
         end
     end
--- tb/async_fifo_tb.sv
+++ tb/async_fifo_tb.sv
@@ -3,7 +3,6 @@
 module async_fifo_tb;
   
     // Parameters
-    parameter CLK_PERIOD = 10; // 10ns clock period (100MHz clock)
     parameter DATA_WIDTH = 8;
     parameter ADDR_WIDTH = 4;
 
@@ -12,28 +11,88 @@ module async_fifo_tb;
     logic rclk;
     logic write_enable;
     logic read_enable;
-    logic [ADDR_WIDTH:0] fifo_occu_in;
-    logic [ADDR_WIDTH:0] fifo_occu_out;
+    logic full;
+    logic empty;
     logic [DATA_WIDTH-1:0] write_data_in;
     logic [DATA_WIDTH-1:0] read_data_out;
 
     async_fifo #(
-      DATA_WIDTH,
-      ADDR_WIDTH
+        DATA_WIDTH,
+        ADDR_WIDTH
     ) DUT (
-      .reset(reset),
-      .wclk(wclk),
-      .rclk(rclk),
-      .write_enable(write_enable),
-      .read_enable(read_enable),
-      .fifo_occu_in(fifo_occu_in),
-      .fifo_occu_out(fifo_occu_out),
-      .write_data_in(write_data_in),
-      .read_data_out(read_data_out)
+        .reset(reset),
+        .wclk(wclk),
+        .rclk(rclk),
+        .write_enable(write_enable),
+        .read_enable(read_enable),
+        .full(full),
+        .empty(empty),
+        .write_data_in(write_data_in),
+        .read_data_out(read_data_out)
     );
+
+    // Asynchronous clocks
+    always #10ns wclk = ~wclk;
+    always #35ns rclk = ~rclk;
+
+    // Variable to hold pushed data
+    reg [DATA_WIDTH-1:0] write_data_q[$], write_data;
+
+    // Indicates that test can stop
+    logic write_done;
+
+    initial begin
+        reset = 1;
+        write_done = 0;
+
+        // Reset pulse
+        #20 reset = 0;
+    end
   
     initial begin
-      $display("TODO");
-      $finish;
+        // Initialize Signals
+        wclk = 0;
+        write_data_in = 0;
+        write_enable = 0;
+        #20;
+
+        repeat(5) begin
+            for (int i=0; i<100; i++) begin
+                @(posedge wclk iff !full);
+                write_enable = (i%2 == 0)? 1'b1 : 1'b0;
+                if (write_enable) begin
+                    write_data_in = $urandom[DATA_WIDTH-1:0];
+                    write_data_q.push_back(write_data_in);
+                end
+            end
+            #50;
+        end
+        write_done = 1;
+    end
+
+    initial begin
+        // Initialize Signals
+        rclk = 0;
+        read_enable = 0;
+        #20;
+
+        repeat(5) begin
+            for (int i=0; i<100; i++) begin
+                @(posedge rclk iff !empty || (empty && write_done));
+                read_enable = (i%2 == 0)? 1'b1 : 1'b0;
+                if (read_enable) begin
+                    write_data = write_data_q.pop_front();
+                    if(read_data_out !== write_data) $error("Time = %0t: Comparison Failed: expected write_data = %h, read_data_out = %h", $time, write_data, read_data_out);
+                    else $display("Time = %0t: Comparison Passed: write_data = %h and read_data_out = %h",$time, write_data, read_data_out);
+                end
+            end
+            #50;
+        end
+        $finish;
+    end
+
+    initial begin
+        $dumpfile("async_fifo_tb.vcd");
+        $dumpvars;
     end
 endmodule
